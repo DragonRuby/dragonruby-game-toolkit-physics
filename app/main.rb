7ccdc99399054569202f10eac14b878727fc96a8
Samuel Ardoin <samuelardoin@gmail.com>
2020-09-20 21:50:50 -0500

    incorporated master branch

--- app/main.rb
+++ app/main.rb
@@ -56,7 +56,7 @@ end
 
 class Paddle
   def initialize ()
-    @x=10
+    @x=WIDTH/2
     @y=100
     @width=100
     @height=20
@@ -81,7 +81,9 @@ class Paddle
       end
     end
 
-    @x = (@x+@width > WIDTH) ? WIDTH-@width : (@x<0) ? 0 : @x;
+    xmin =WIDTH/4
+    xmax = 3*(WIDTH/4)
+    @x = (@x+@width > xmax) ? xmax-@width : (@x<xmin) ? xmin : @x;
   end
 
   def render args
@@ -101,7 +103,7 @@ class Ball
   #@xy [Vector2d] x,y position
   #@velocity [Vector2d] velocity of ball
   def initialize
-    @xy = Vector2d.new(300,500)
+    @xy = Vector2d.new(WIDTH/2,500)
     @velocity = Vector2d.new(2,2)
     @width =  20
     @height = 20
@@ -117,7 +119,7 @@ class Ball
   #render the ball to the screen
   def render args
     args.outputs.solids << [@xy.x,@xy.y,@width,@height,255,0,255];
-    args.outputs.labels << [100,HEIGHT-50,"velocity: " +@velocity.x.to_s+","+@velocity.y.to_s]
+    args.outputs.labels << [20,HEIGHT-50,"velocity: " +@velocity.x.to_s+","+@velocity.y.to_s]
   end
 
   def rect
@@ -258,23 +260,92 @@ class LinearCollider
   end
 end
 
+#Method to init default values
+def defaults args
+  args.state.game_board ||= [(WIDTH / 2 - WIDTH / 4), 0, (WIDTH / 2), HEIGHT]
+  args.state.bricks ||= []
+  args.state.num_bricks ||= 0
+end
 
+#Render loop
+def render args
+  render_instructions args
+  render_board args
+  render_bricks args
+
+
+end
+
+begin :render_methods
+
+  #Method to display the instructions of the game
+  def render_instructions args
+    args.outputs.labels << [225, HEIGHT - 30, "S and D to move the paddle left and right",  0, 1]
+  end
+
+  def render_board args
+    args.outputs.borders << args.state.game_board
+  end
+
+  def render_bricks args
+    args.outputs.solids << args.state.bricks.map(&:rect)
+  end
+
+  def add_new_bricks args
+    return if args.state.num_bricks > 40
+
+    #Width of the game board is 640px
+    brick_width = (WIDTH / 2) / 10
+    brick_height = brick_width / 2
+    x = 0
+    y = 0
+
+    while y < 4
+      #Make a box that is 10 bricks wide and 4 bricks tall
+      args.state.bricks += (10).map do
+        args.state.new_entity(:brick) do |b|
+          b.x = x * brick_width + (WIDTH / 2 - WIDTH / 4)
+          b.y = HEIGHT - ((y + 1) * brick_height)
+          b.rect = [b.x + 1, b.y - 1, brick_width - 2, brick_height - 2, 135, 135, 135]
+          args.state.num_bricks += 1
+          x += 1
+        end
+      end
+      x = 0
+      y += 1
+    end
+  end
+end
+
+#Calls all methods necessary for performing calculations
+def calc args
+  add_new_bricks args
+end
+
+$mode = :both
 def tick args
-  args.state.paddle ||= Paddle.new
-  args.state.ball   ||= Ball.new
-  args.state.westWall  ||= LinearCollider.new([0,0],[0,HEIGHT], :pos)
-  args.state.eastWall  ||= LinearCollider.new([WIDTH,0],[WIDTH,HEIGHT])
-  args.state.southWall ||= LinearCollider.new([0,0],[WIDTH,0])
-  args.state.northWall ||= LinearCollider.new([0,HEIGHT],[WIDTH,HEIGHT],:pos)
+  if $mode == :paddle || $mode == :both
+      args.state.paddle ||= Paddle.new
+      args.state.ball   ||= Ball.new
+      args.state.westWall  ||= LinearCollider.new([WIDTH/4,0],[WIDTH/4,HEIGHT], :pos)
+      args.state.eastWall  ||= LinearCollider.new([3*WIDTH*0.25,0],[3*WIDTH*0.25,HEIGHT])
+      args.state.southWall ||= LinearCollider.new([0,0],[WIDTH,0])
+      args.state.northWall ||= LinearCollider.new([0,HEIGHT-32*4],[WIDTH,HEIGHT-32*4],:pos)
 
-  args.state.paddle.update args
-  args.state.ball.update args
+      args.state.paddle.update args
+      args.state.ball.update args
 
-  args.state.westWall.update args
-  args.state.eastWall.update args
-  args.state.southWall.update args
-  args.state.northWall.update args
+      args.state.westWall.update args
+      args.state.eastWall.update args
+      args.state.southWall.update args
+      args.state.northWall.update args
 
-  args.state.paddle.render args
-  args.state.ball.render args
+      args.state.paddle.render args
+      args.state.ball.render args
+  end
+  if $mode == :brick || $mode == :both
+    defaults args
+    render args
+    calc args
+  end
 end
