597d43cc67d1f1c8962bc5c48e7e90b70ef25287
Samuel Ardoin <samuelardoin@gmail.com>
2020-11-09 18:49:39 -0600

    empty canvas:

--- app/peg.rb
+++ app/peg.rb
@@ -1,16 +1,2 @@
 class Peg
-    def initialize(x, y, block_size)
-        @x = x
-        @y = y
-        @block_size = block_size
-
-        @r = 255
-        @g = 0
-        @b = 0
-   end
-
-   def draw(args)
-    #Offset the coordinates to the edge of the game area 
-    args.outputs.solids << [@x, @y, @block_size, @block_size, @r, @g, @b]
-   end
 end
